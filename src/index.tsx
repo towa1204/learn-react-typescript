c147d5c44205301361c2c2fac40359852f2a5145
towa1204 <lsadsfj@gmai.com>
2022-06-26 11:17:26 +0900

    4. コンポーネントとprops(2)

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,17 @@ function Welcome(props: {name: string}) {
   return <h1>Hello, {props.name}</h1>;
 }
 
-const element = <Welcome name="Sara" />;
-root.render(element);
+function AppWelcome() {
+  return (
+    <div>
+      <Welcome name="桃山みらい" />
+      <Welcome name="萌黄えも" />
+      <Welcome name="青葉りんか" />
+    </div>
+  );
+}
+
+root.render(<AppWelcome />);
 
 
 // If you want to start measuring performance in your app, pass a function
