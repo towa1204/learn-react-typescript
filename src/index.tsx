0243c059ebf368f14b461cd1b3ea0a8ec776ec54
towa1204 <lsadsfj@gmai.com>
2022-06-26 10:57:36 +0900

    2. JSXの導入

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,25 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+type User = {firstName: string, lastName: string};
+
+function formatName(user: User) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez',
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
 );
+
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
