6742a16aac7f7d57cbbecf0461d09a3d13eaa700
towa1204 <lsadsfj@gmai.com>
2022-06-26 12:52:08 +0900

    5. stateとライフサイクル(2)

--- src/index.tsx
+++ src/index.tsx
@@ -8,72 +8,20 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-function formatDate(date: Date) {
-  return date.toLocaleDateString();
-}
-
-type CommentObj = {
-  date: Date,
-  text: string,
-  author: User,
-};
-
-type User = {
-  avatarUrl: string,
-  name: string,
-};
-
-function Avatar(props: {user: User}) {
+function Clock(props: {date: Date}) {
   return (
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  );
-}
-
-function UserInfo(props: {user: User}) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
     </div>
   );
 }
 
-function Comment(props: CommentObj) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
+function tick() {
+  root.render(<Clock date={new Date()}/>);
 }
 
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64'
-  }
-};
-
-root.render(
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author} />
-);
-
-
+setInterval(tick, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
