80af3d335399524e319aa958cd83c95cc9d60c4f
towa1204 <lsadsfj@gmai.com>
2022-06-27 17:54:00 +0900

    5. stateとライフサイクル(7)

--- src/index.tsx
+++ src/index.tsx
@@ -47,7 +47,13 @@ class Clock extends React.Component<{}, ClockState> {
   }
 }
 
-root.render(<Clock />);
+root.render(
+  <div>
+    <Clock />
+    <Clock />
+    <Clock />
+  </div>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
