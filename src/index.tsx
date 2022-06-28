c022306de2d5e8c60fb4fa184dbe6788f0f6ee54
towa1204 <lsadsfj@gmai.com>
2022-06-28 21:04:55 +0900

    7.条件付きレンダー(3)

--- src/index.tsx
+++ src/index.tsx
@@ -8,74 +8,24 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-function UserGreeting(props: {}) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props:{}) {
-  return <h1>Please Sign Up.</h1>;
-}
-
-function Greeting(props: {isLoggedIn: boolean}) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
-
-function LoginButton(props: {onClick: () => void}) {
+function Mailbox(props: {unreadMessages: string[]}) {
+  const unreadMessages = props.unreadMessages;
   return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
   );
 }
 
-function LogoutButton(props: {onClick: () => void}) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
-}
+// const messages = ['React', 'Re: React', 'Re:Re: React'];
+const messages: string[] = [];
 
-class LoginControl extends React.Component<{}, {isLoggedIn: boolean}> {
-  constructor(props: {}) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
-
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
-    );
-  }
-}
-
-
-root.render(<LoginControl />);
+root.render(<Mailbox unreadMessages={messages}/>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
