77419802ca74d3fbb184d93cd1619aa6979751c1
towa1204 <lsadsfj@gmai.com>
2022-07-01 03:13:09 +0900

    8. リストとkey(4)

--- src/index.tsx
+++ src/index.tsx
@@ -8,23 +8,38 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-function ListItem(props: {value: number}) {
-  return <li>{props.value}</li>
-}
-
-function NumberList(props: {numbers: number[]}) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) => 
-    <ListItem key={number.toString()} value={number} />
+function Blog(props: {posts: {id: number, title: string, content: string}[]}) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) => 
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) => 
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
   );
 
-  return (<ul>{listItems}</ul>);
 }
 
-const numbers = [1, 2, 3, 4, 5];
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
 
-
-root.render(<NumberList numbers={numbers} />);
+root.render(<Blog posts={posts} />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
