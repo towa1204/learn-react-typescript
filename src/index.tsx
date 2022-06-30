c758e9b7a3924823b3c0ea41e2985fd50da18b49
towa1204 <lsadsfj@gmai.com>
2022-07-01 02:49:48 +0900

    8.リストとkey(1)

--- src/index.tsx
+++ src/index.tsx
@@ -8,44 +8,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-function WarningBanner(props: {warn: boolean}) {
-  // warnがfalseのときはレンダリングしない
-  if (!props.warn) {
-    return null;
-  }
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) => <li>{number}</li>);
 
-  return (
-    <div className="warning">
-      Warning!
-    </div>
-  );
-}
-
-class Page extends React.Component<{}, {showWarning: boolean}> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {showWarning: true};
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  handleToggleClick() {
-    this.setState(state => ({
-      showWarning: !state.showWarning
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    );
-  }
-}
-root.render(<Page />);
+root.render(<ul>{listItems}</ul>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
