f0efe656d7f5f45de3c69b27c4d080e034642481
towa1204 <lsadsfj@gmai.com>
2022-07-01 02:50:16 +0900

    8. リストとkey(2)

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,17 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number) => <li>{number}</li>);
+function NumberList(props: {numbers: number[]}) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => <li>{number}</li>);
 
-root.render(<ul>{listItems}</ul>);
+  return (<ul>{listItems}</ul>);
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+
+root.render(<NumberList numbers={numbers} />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
