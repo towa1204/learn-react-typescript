4064b5d0d370b9d5b15f88f4cf88d426da2144bb
towa1204 <lsadsfj@gmai.com>
2022-07-01 16:18:48 +0900

    9. フォーム(2)

--- src/index.tsx
+++ src/index.tsx
@@ -8,21 +8,24 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-class NameForm extends React.Component<{}, {value: string}> {
+
+class EssayForm extends React.Component<{}, {value: string}> {
   constructor(props: {}) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
     this.setState({value: event.target.value});
   }
 
   handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('An essay was submitted: ' + this.state.value);
     event.preventDefault();
   }
 
@@ -30,15 +33,16 @@ class NameForm extends React.Component<{}, {value: string}> {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
-        <input type="submit" value="submit" />
+        <input type="submit" value="Submit" />
       </form>
     );
   }
 }
-root.render(<NameForm />);
+
+root.render(<EssayForm />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
